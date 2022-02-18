d6ae1d364f2230c7b7332bfcdaf068c6ac0b4241
Mewtwo22 <22lwalls@ga.usmk12.org>
2022-02-18 16:26:03 -0600

    Added red/blue different auto

--- src/main/java/frc/robot/commands/AutoCommandGroup.java
+++ src/main/java/frc/robot/commands/AutoCommandGroup.java
@@ -3,16 +3,15 @@ package frc.robot.commands;
 //Imports for commands and subsystems used
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.NavXHandler;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.ConveyorSubsystem;
-import frc.robot.subsystems.LimeLightSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 
 public class AutoCommandGroup extends SequentialCommandGroup {
 
     //Declare subsystems and NavX used
-    private LimeLightSubsystem limeLightSubsystem;
     private SwerveDriveSubsystem swerveDriveSubsystem;
     private ShooterSubsystem shooterSubsystem;
     private ConveyorSubsystem conveyorSubsystem;
@@ -20,20 +19,23 @@ public class AutoCommandGroup extends SequentialCommandGroup {
 
     public AutoCommandGroup(RobotContainer robotContainer) {
         //Instantiate subsystems and NavX; set limelight to desired pipeline
-        limeLightSubsystem = robotContainer.getLimeLightSubsystem();
         swerveDriveSubsystem = robotContainer.getSwervedriveSubsystem();
         shooterSubsystem = robotContainer.getShooterSubsystem();
-        limeLightSubsystem.setPipeline(1);
-        navXHandler = robotContainer.getNavXHandler();
-
-        RotateToZero.setInitialAngle(navXHandler.getAngleRad());
-        RotateToAngle.setInitialAngle(navXHandler.getAngleRad());
-
+        
         //Add each command you want the robot to do in order
-        addCommands(new GoBackwards(swerveDriveSubsystem));
-        addCommands(new AutoShoot(shooterSubsystem));
-        addCommands(new Wait(1));
-        addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+        if (Robot.PREFS.getString("Alliance Color", "Red").toUpperCase().equals("RED")){
+            addCommands(new GoBackwards(swerveDriveSubsystem));
+            addCommands(new AutoShoot(shooterSubsystem));
+            addCommands(new Wait(1));
+            addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+            addCommands(new RotateToAngle(swerveDriveSubsystem, navXHandler, Math.PI/2));
+        }else{
+            addCommands(new GoBackwards(swerveDriveSubsystem));
+            addCommands(new AutoShoot(shooterSubsystem));
+            addCommands(new Wait(1));
+            addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+            addCommands(new RotateToAngle(swerveDriveSubsystem, navXHandler, Math.PI/2));
+        }
     }
 
 }
