7ad96b9d8fc07a2987770283e06043e0465d5ff7
williamchen <chenbwilliam@gmail.com>
2022-03-03 16:49:43 -0600

    potentially fixed negative values

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -16,24 +16,24 @@ public class IntakeSubsystem extends SubsystemBase {
 
     private CANSparkMax angleMotor;
     private CANSparkMax intakeMotor;
-    //private RelativeEncoder angleEncoder;
+    // private RelativeEncoder angleEncoder;
     private double inTakePower;
     private double anglePower;
-    //private int offset;
+    // private int offset;
     private SimpleWidget intakeWidget;
     private SimpleWidget angleWidget;
 
     public IntakeSubsystem() {
         this.angleMotor = new CANSparkMax(10, CANSparkMaxLowLevel.MotorType.kBrushless);
         this.intakeMotor = new CANSparkMax(9, CANSparkMaxLowLevel.MotorType.kBrushless);
-        //this.angleEncoder = angleMotor.getEncoder();
-        intakeWidget = Shuffleboard.getTab("Preferences").add("intakePercent", -0.433)
-        .withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("min", 0, "max", 1));
-        angleWidget = Shuffleboard.getTab("Preferences").add("hammerPercent", -0.4)
-        .withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("min", 0, "max", 1));
-        //offset = 0;
+        // this.angleEncoder = angleMotor.getEncoder();
+        intakeWidget = Shuffleboard.getTab("Preferences").add("intakePercent", 0.433)
+                .withWidget(BuiltInWidgets.kNumberSlider)
+                .withProperties(Map.of("min", 0, "max", 1));
+        angleWidget = Shuffleboard.getTab("Preferences").add("hammerPercent", 0.4)
+                .withWidget(BuiltInWidgets.kNumberSlider)
+                .withProperties(Map.of("min", 0, "max", 1));
+        // offset = 0;
 
         SendableRegistry.addChild(this, angleMotor);
         SendableRegistry.addChild(this, intakeMotor);
@@ -42,34 +42,34 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void backIntake() {
-        intakeMotor.set(-inTakePower);
-    }
-
-    public void backAngle() {
-        //if (angleEncoder.getPosition() > offset && !backLimit.get()){
-            angleMotor.set(-anglePower);
-        //}else{
-            //stopAngle();
-        //}
-    }
-
-    public void intake() {
         intakeMotor.set(inTakePower);
     }
 
+    public void backAngle() {
+        // if (angleEncoder.getPosition() > offset && !backLimit.get()){
+        angleMotor.set(anglePower);
+        // }else{
+        // stopAngle();
+        // }
+    }
+
+    public void intake() {
+        intakeMotor.set(-inTakePower);
+    }
+
     public void angle() {
-        //if (angleEncoder.getPosition() < offset + 100 && !frontLimit.get()){
-            angleMotor.set(anglePower*1.5);
-        //}else{
-            //stopAngle();
-        //}
+        // if (angleEncoder.getPosition() < offset + 100 && !frontLimit.get()){
+        angleMotor.set(-anglePower * 1.5);
+        // }else{
+        // stopAngle();
+        // }
     }
 
     public void stopIntake() {
         intakeMotor.set(0);
     }
 
-    public void stopAngle(){
+    public void stopAngle() {
         angleMotor.set(0);
     }
 
