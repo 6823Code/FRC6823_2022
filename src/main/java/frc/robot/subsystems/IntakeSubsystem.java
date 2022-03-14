adc1fff685e66f373993fe0b7a8f8a5241cb2a17
6823Code <usmrobotics@ga.usmk12.org>
2022-03-14 07:54:27 -0500

    Removed pid from hammer

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 
-import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
@@ -15,19 +15,19 @@ import java.util.Map;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-    private final double P = .04;
-    private final double I = .00001;
+    // private final double P = .04;
+    // private final double I = .00001;
     private CANSparkMax angleMotor;
     private CANSparkMax intakeMotor;
     private DutyCycleEncoder angleEncoder;
     private double inTakePower;
     private double anglePower;
-    private double margin;
-    private double downPos;
-    private double pidPower;
+    // private double margin;
+    // private double downPos;
+    // private double pidPower;
     private SimpleWidget intakeWidget;
     private SimpleWidget angleWidget;
-    private PIDController pid;
+    //private PIDController pid;
 
     public IntakeSubsystem() {
         this.angleMotor = new CANSparkMax(10, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -39,10 +39,10 @@ public class IntakeSubsystem extends SubsystemBase {
         angleWidget = Shuffleboard.getTab("Preferences").addPersistent("anglePercent", 0.433)
                 .withWidget(BuiltInWidgets.kNumberSlider)
                 .withProperties(Map.of("min", 0, "max", 1));
-        margin = 0.1;
-        downPos = 0.32;
+        // margin = 0.1;
+        // downPos = 0.32;
 
-        pid = new PIDController(P, I, 0);
+        // pid = new PIDController(P, I, 0);
 
         SendableRegistry.addChild(this, angleMotor);
         SendableRegistry.addChild(this, intakeMotor);
@@ -55,10 +55,11 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void angle() {
-        while(Math.abs(angleEncoder.getAbsolutePosition() - downPos) > margin){
-            pidPower = pid.calculate(angleEncoder.getAbsolutePosition(), downPos);
-            angleMotor.set(-pidPower);
-        }
+        // while(Math.abs(angleEncoder.getAbsolutePosition() - downPos) > margin){
+        //     pidPower = pid.calculate(angleEncoder.getAbsolutePosition(), downPos);
+        //     angleMotor.set(-pidPower);
+        // }
+        angleMotor.set(-1.5 * anglePower);
     }
 
     public void intake() {
