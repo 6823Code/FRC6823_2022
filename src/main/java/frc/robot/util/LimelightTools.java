375bc756cffe28e8408d7c2da9c8d9d4f283b602
6823Code <usmrobotics@ga.usmk12.org>
2022-04-02 11:21:11 -0500

    Updated trigonometry in distFromTower

--- src/main/java/frc/robot/util/LimelightTools.java
+++ src/main/java/frc/robot/util/LimelightTools.java
@@ -1,10 +1,11 @@
 package frc.robot.util;
 
 public class LimelightTools {
-    public static double distFromTower(double angle){ //returns horizontal distance in meters, angle from -0.5 to 0.5, should only receive values between zero and 0.25
+    public static double distFromTower(double angle){ //returns horizontal distance in meters, angle in radians
         final double HEIGHT = 2.6416;
         final double LL_HEIGHT = 0.7;
         double heightDiff = HEIGHT - LL_HEIGHT;
+        angle += Math.PI * 50 / 180;
         // return Math.sin((0.25 * Math.PI) - angle) * heightDiff / Math.sin(angle); //Law of sines
         return heightDiff / Math.tan(angle);
     }
