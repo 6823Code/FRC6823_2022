595825747a0e8f77c0aefca53b7ae44e4174fc8a
6823Code <usmrobotics@ga.usmk12.org>
2022-05-25 16:47:56 -0500

    yo

--- src/main/java/frc/robot/commands/AutoCommandGroup.java
+++ src/main/java/frc/robot/commands/AutoCommandGroup.java
@@ -12,7 +12,7 @@ import frc.robot.subsystems.SwerveDriveSubsystem;
 
 public class AutoCommandGroup extends SequentialCommandGroup {
 
-    //Declare subsystems and NavX used
+    // Declare subsystems and NavX used
     private SwerveDriveSubsystem swerveDriveSubsystem;
     private ShooterSubsystem shooterSubsystem;
     private ConveyorSubsystem conveyorSubsystem;
@@ -21,33 +21,43 @@ public class AutoCommandGroup extends SequentialCommandGroup {
     // private NavXHandler navXHandler;
 
     public AutoCommandGroup(RobotContainer robotContainer) {
-        //Instantiate subsystems and NavX; set limelight to desired pipeline
+        // Instantiate subsystems and NavX; set limelight to desired pipeline
         swerveDriveSubsystem = robotContainer.getSwervedriveSubsystem();
         shooterSubsystem = robotContainer.getShooterSubsystem();
         conveyorSubsystem = robotContainer.getConveyorSubsystem();
         setIntakeSubsystem(robotContainer.getIntakeSubsystem());
         // limeLightSubsystem = robotContainer.getLimeLightSubsystem();
-        
-        //Add each command you want the robot to do in order
-            addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 0.5));
-            addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
-            addCommands(new Wait(3));
 
-            addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 0.5));
-            addCommands(new Wait(3));
-            addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
-            addCommands(new Wait(3));
+        // Add each command you want the robot to do in order
+        addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 0.5));
+        addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+        addCommands(new Wait(3));
 
-            addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 0.5));
-            addCommands(new AutoShoot(shooterSubsystem, conveyorSubsystem, 0, 0.6, 1500, 1500));
-            addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
-            addCommands(new Wait(3));
+        addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 0.5));
+        addCommands(new Wait(3));
+        addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+        addCommands(new Wait(3));
 
-            addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 0.5));
-            addCommands(new AutoShoot(shooterSubsystem, conveyorSubsystem, 0, 0.6, 1500, 1500));
-            addCommands(new Wait(3));
-            addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
-        }
+        addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 0.5));
+        addCommands(new AutoShoot(shooterSubsystem, conveyorSubsystem, 0, 0.6, 1500, 1500));
+        addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+        addCommands(new Wait(3));
+
+        addCommands(new GoBackwards(swerveDriveSubsystem, 0.1, 3));
+        addCommands(new AutoShoot(shooterSubsystem, conveyorSubsystem, 0, 0.6, 1500, 1500));
+        addCommands(new Wait(3));
+        addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+        addCommands(new Wait(3));
+
+        addCommands(new AutoShoot(shooterSubsystem, conveyorSubsystem, 0, 0.6, 1500, 1500));
+        addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+        addCommands(new Wait(3));
+
+        addCommands(new AutoShoot(shooterSubsystem, conveyorSubsystem, 0, 0.6, 1500, 1500));
+        addCommands(new Wait(3));
+        addCommands(new Halt(swerveDriveSubsystem, shooterSubsystem, conveyorSubsystem));
+        addCommands(new Wait(3));
+    }
 
     public IntakeSubsystem getIntakeSubsystem() {
         return intakeSubsystem;
