8bbc623e0442e627dafd25a07cb3a1e037502ac1
BillyumBoi <24wzhao@ga.usmk12.org>
2022-05-25 10:30:25 -0500

    Auto input taking

---
+++ src/main/java/frc/robot/AutoGet/AutoGet.java
@@ -0,0 +1,27 @@
+package frc.robot.AutoGet;
+
+import java.util.Timer;
+
+public class AutoGet {
+
+    private Timer timer;
+    private AutoTask task;
+
+    // Initializes Timer and AutoTask with joystick number (usually 3)
+    public AutoGet(int joyNum)
+    {
+        timer = new Timer(true);
+        task = new AutoTask(joyNum);
+    }
+
+    // Runs the run() function from AutoTask at regular intervals to get inputs
+    // Delay is the delay after teleop has begun to start taking inputs
+    // Period is the time between getting inputs (measured in miliseconds)
+    // (16 ms ~ 60 inputs per second)
+    public boolean start(long delay)
+    {
+        System.out.println("Started!");
+        timer.scheduleAtFixedRate(task, delay, 16);
+        return true;
+    }
+}
---
+++ src/main/java/frc/robot/AutoGet/AutoTask.java
@@ -0,0 +1,68 @@
+package frc.robot.AutoGet;
+
+//For file writing and getting time
+import java.util.*;
+import java.io.*;
+
+// For getting controller input
+import frc.robot.JoystickHandler;
+
+
+
+
+public class AutoTask  extends TimerTask{
+    private Date s;
+    private long holdTime;
+    private JoystickHandler stick;
+    private File file;
+
+
+    /**
+     * Constructor for objects of class TimerStuff
+     */
+    public AutoTask(int num)
+    {
+        s = new Date();
+        holdTime = 0;
+        stick = new JoystickHandler(num);
+        file = new File("autoCommands.txt");
+        try {
+            file.createNewFile();
+        } catch (Exception e) {
+            
+        }
+    }
+
+    public void run()
+    {
+        Date e = new Date();
+        System.out.println(e.getTime() - s.getTime());
+        holdTime = e.getTime() - s.getTime();
+        if (holdTime > 29000)
+        {
+            cancel();
+        }
+        if (stick.isJoystickInUse())
+        {
+            file(stick.getAxis0(), stick.getAxis1(), stick.getAxis2(), stick.getAxis3(), stick.getAxis4(), stick.getAxis5(), stick.getAxis6(), getHold());
+        }
+    }
+
+    public long getHold()
+    {
+        return holdTime;
+    }
+
+
+    public void file(double a0, double a1, double a2, double a3, double a4, double a5, double a6, long time)
+    {
+        try {
+            FileWriter out = new FileWriter(file);
+            out.write(a0 + "," + a1 + "," + a2 + "," + a3 + "," + a4 + "," + a5 + "," + a6 + "," + time + ",");
+            out.close();
+        } catch (Exception e) {
+            
+        }
+        
+    }
+}
