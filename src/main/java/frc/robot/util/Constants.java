bcc34dd5cc2b94b378dd77421ded380b419bfdf0
ryanmbruce <ryanmbruce@gmail.com>
2022-06-27 17:59:00 -0500

    Switched to constant.java bot dimensions

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -1,10 +1,40 @@
 package frc.robot.util;
 
 public class Constants {
+    
+    //
+    // Fundamental Constants
+    //
     public static final double degToRad = Math.PI / 180;
     public static final double radToDeg = 180 / Math.PI;
-    public static final double fLOffset = 171.474609375;
-    public static final double fROffset = 88.857421875;
-    public static final double bLOffset = 301.728515625;
-    public static final double bROffset = 165.76171875;
+    
+    
+    //
+    // Robot-Specific Constants
+    //
+        public static final double fLOffset = 171.474609375;
+        public static final double fROffset = 88.857421875;
+        public static final double bLOffset = 301.728515625;
+        public static final double bROffset = 165.76171875;
+        public static final double RobotWidth = 1;
+        public static final double RobotLength = 1;
+
+
+    //
+    // Limelight Constants
+    //
+
+
+    //
+    // Field-Specific Constants
+    //
+
+
+    //
+    //Game-specific Constants
+    //
+
+        // UpperHubHeight = XXXX
+        // LowerHubHeight = XXXX
+
 }
